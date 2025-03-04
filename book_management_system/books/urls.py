3cf8d6397aa56122e6d9b9ff76fd196d82f8eebb
Shad-islam <m.shadislam01@gmail.com>
2025-03-05 05:31:46 +0600

    book add feature added

--- book_management_system/books/urls.py
+++ book_management_system/books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage,add_book
 
 urlpatterns = [
    path('',homepage,name='homepage'),
+   path('add_book/',add_book,name='add_book'),
 ]
--- book_management_system/books/views.py
+++ book_management_system/books/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Book
+from .forms import BookForm
 
 # Create your views here.
 def homepage(request):
     books = Book.objects.all()
-    return render(request,'book-list.html',{'books':books})
-   
+    return render(request,'book_list.html',{'books':books})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return redirect('homepage')
+    else:
+        print('get the form') 
+        form = BookForm()
+        return render(request,'add_book.html', {'form':form})
